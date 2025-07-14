3748482cb7ed704c38ba36510f281521147dac48
Geronimo <thegeronimo@protonmail.com>
2025-07-14 15:32:57 -0400

    Revert "fix hot reloading"
    state should persist between reloads
    
    This reverts commit 878fb32f6fd7b5eee8e9a5d4de4aa0cb51a70702.

--- src/hyperopen/core.cljs
+++ src/hyperopen/core.cljs
@@ -1,8 +1,8 @@
 (ns hyperopen.core
   (:require [replicant.dom :as d]))
 
-(def app-state (atom {:title "Hyperopen"
-                          :message "Welcome to Hyperopen - A ClojureScript app with Replicant!!!!"
+(defonce app-state (atom {:title "Hyperopen"
+                          :message "Welcome to Hyperopen - A ClojureScript app with Replicant"
                           :count 0}))
 
 (defn app-view [state]
@@ -18,15 +18,7 @@
   (d/render (.getElementById js/document "app")
             (app-view @app-state)))
 
-;; Called on first load and *after* every hot-reload
-(defn ^:dev/after-load start []
-  ;; (re)add any watches if needed; here we simply re-render
-  (render!)
-  (println "Hyperopen re-rendered after hot-reload"))
-
 (defn init []
   (println "Initializing Hyperopen...")
-  ;; Watch state so UI updates on state changes
   (add-watch app-state :render (fn [_ _ _ _] (render!)))
-  ;; First render
-  (start)) 
+  (render!)) 
