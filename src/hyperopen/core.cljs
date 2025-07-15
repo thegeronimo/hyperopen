f1fbc94344dcb3f4c3063523cc426fa9d6cb0a79
Geronimo <thegeronimo@protonmail.com>
2025-07-15 14:34:16 -0400

    add main.css + concurrently run tailwind + shadow watch

--- src/hyperopen/core.cljs
+++ src/hyperopen/core.cljs
@@ -6,13 +6,15 @@
                           :count 0}))
 
 (defn app-view [state]
-  [:div
-   [:h1 (:title state)]
-   [:p (:message state)]
-   [:div
-    [:p "You clicked " (:count state) " times"]
-    [:button {:on {:click #(swap! app-state update :count inc)}}
-     "Click me!"]]])
+  [:div.min-h-screen.flex.flex-col.items-center.justify-center.bg-base-100.p-8
+   [:div.text-center.space-y-6.max-w-md
+    [:h1.text-4xl.font-bold.text-primary (:title state)]
+    [:p.text-lg.text-base-content.opacity-80 (:message state)]
+    [:div.card.bg-base-200.shadow-xl.p-6
+     [:p.text-xl.mb-4 "You clicked " (:count state) " times!!"]
+     [:button.btn.btn-primary.btn-lg
+      {:on {:click #(swap! app-state update :count inc)}}
+      "Click me!"]]]])
 
 (defn render! []
   (d/render (.getElementById js/document "app")
