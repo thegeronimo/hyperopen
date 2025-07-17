be9cf01f87a54fd6f77d60d2437e5900cab2a907
Geronimo <thegeronimo@protonmail.com>
2025-07-16 20:19:02 -0400

    feat: implement l2Book order book subscription
    - Add orderbook.cljs module with subscribe/unsubscribe functionality
    - Handle Hyperliquid l2Book data format (bids/asks arrays)
    - Integrate with existing WebSocket client infrastructure

---
+++ src/hyperopen/websocket/orderbook.cljs
@@ -0,0 +1,77 @@
+(ns hyperopen.websocket.orderbook
+  (:require [hyperopen.websocket.client :as ws-client]))
+
+;; Order book state
+(defonce orderbook-state (atom {:subscriptions #{}
+                                :books {}})) ; Map of coin -> {:bids [...] :asks [...]}
+
+;; Subscribe to order book for a symbol
+(defn subscribe-orderbook! [symbol]
+  (when (ws-client/connected?)
+    (let [subscription-msg {:method "subscribe"
+                            :subscription {:type "l2Book"
+                                           :coin symbol}}]
+      (ws-client/send-message! subscription-msg)
+      (swap! orderbook-state update :subscriptions conj symbol)
+      (println "Subscribed to order book for:" symbol))))
+
+;; Unsubscribe from order book for a symbol
+(defn unsubscribe-orderbook! [symbol]
+  (when (ws-client/connected?)
+    (let [unsubscription-msg {:method "unsubscribe"
+                              :subscription {:type "l2Book"
+                                             :coin symbol}}]
+      (ws-client/send-message! unsubscription-msg)
+      (swap! orderbook-state update :subscriptions disj symbol)
+      (swap! orderbook-state update :books dissoc symbol)
+      (println "Unsubscribed from order book for:" symbol))))
+
+;; Handle incoming order book data
+(defn handle-orderbook-data! [data]
+  (println "Processing order book data:" data)
+  (when (and (map? data) (= (:channel data) "l2Book"))
+    (let [book-data (:data data)
+          coin (:coin book-data)
+          levels (:levels book-data)]
+      (when (and coin levels (>= (count levels) 2))
+        ;; Hyperliquid l2Book format: levels = [bids_array, asks_array]
+        (let [bids (first levels)   ; First array is bids
+              asks (second levels)] ; Second array is asks
+          (swap! orderbook-state assoc-in [:books coin] 
+                 {:bids (vec (sort-by :px > bids))  ; Sort bids highest to lowest
+                  :asks (vec (sort-by :px < asks))  ; Sort asks lowest to highest
+                  :timestamp (:time book-data)})
+          (println "Updated order book for" coin 
+                   "- Bids:" (count bids) "Asks:" (count asks)))))))
+
+;; Get current subscriptions
+(defn get-subscriptions []
+  (:subscriptions @orderbook-state))
+
+;; Get order book for a specific symbol
+(defn get-orderbook [symbol]
+  (get-in @orderbook-state [:books symbol]))
+
+;; Get all order books
+(defn get-all-orderbooks []
+  (:books @orderbook-state))
+
+;; Get best bid and ask for a symbol
+(defn get-best-bid-ask [symbol]
+  (when-let [book (get-orderbook symbol)]
+    {:best-bid (first (:bids book))
+     :best-ask (first (:asks book))}))
+
+;; Clear order book data for a specific symbol
+(defn clear-orderbook! [symbol]
+  (swap! orderbook-state update :books dissoc symbol))
+
+;; Clear all order book data
+(defn clear-all-orderbooks! []
+  (swap! orderbook-state assoc :books {}))
+
+;; Initialize order book module
+(defn init! []
+  (println "Order book subscription module initialized")
+  ;; Register handler for l2Book channel
+  (ws-client/register-handler! "l2Book" handle-orderbook-data!)) 
